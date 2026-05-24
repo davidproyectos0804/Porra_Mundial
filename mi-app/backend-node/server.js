8c9d749acb5c3d8886f567f511862f3f061206b2
DavidProyectos0804 <davidproyectos0804@gmail.com>
2026-05-24 06:09:51 +0200

    Cors

--- mi-app/backend-node/server.js
+++ mi-app/backend-node/server.js
@@ -13,15 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware: CORS
-app.use(cors({
-  origin: [
-    'http://localhost:4200',
-    'https://porra-mundial-theta.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.options('*', cors());
 
 // Middleware: Parsear JSON
