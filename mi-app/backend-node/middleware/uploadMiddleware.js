934c44fe8238fbe7cc90007376d240ee4c7cb612
DavidProyectos0804 <davidproyectos0804@gmail.com>
2026-06-14 19:47:52 +0200

    Limite borrado para imagenes

--- mi-app/backend-node/middleware/uploadMiddleware.js
+++ mi-app/backend-node/middleware/uploadMiddleware.js
@@ -5,7 +5,6 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB máximo
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
